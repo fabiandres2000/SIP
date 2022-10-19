ae296eefded20f4ccea7cc9569714539e3cdb4c5
Fabian Quintero <grovveip@gmail.com>
2022-10-19 08:43:03 -0500

    ok

--- resources/js/store.js
+++ resources/js/store.js
@@ -7,8 +7,8 @@ export default new Vuex.Store({
     state: {
         apiURL: 'http://127.0.0.1:8000/',
         serverPath: 'http://127.0.0.1:8000/',
-        // apiURL: 'http://lecsidesarrollos.com.co/SIP/public/',
-        // serverPath: 'http://lecsidesarrollos.com.co/SIP/public/',
+        // apiURL: 'https://lecsidesarrollos.com.co/SIP/public/',
+        // serverPath: 'https://lecsidesarrollos.com.co/SIP/public/',
         // apiURL: 'http://192.168.1.10/SIP/public/',
         // serverPath: 'http://192.168.1.10/SIP/public/',
         // apiURL: 'http://192.168.1.150/SIP/public/',
